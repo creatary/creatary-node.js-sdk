f4e54a863e7c0836068db82626941cc12d7e5295
Balazs Nagy <balazs.1.nagy@nsn.com>
2012-04-06 15:39:58 -0700

    100% code coverage for SMS

--- test/sms.js
+++ test/sms.js
@@ -9,13 +9,12 @@ var sms = test_load("sms.js");
 var shared = test_load("shared.js");
 
 describe('Sms', function() {
-	
+
 	var sandbox = sinon.sandbox.create();
-	
+
 	afterEach(function() {
 		sandbox.restore();
 	});
-	
 	describe(".send", function() {
 		it('should post sms', function() {
 			// given
@@ -33,35 +32,87 @@ describe('Sms', function() {
 			}));
 		})
 	});
-	
 	describe(".receive", function() {
-		
+
 		it("should listen on default", function() {
 			// given
 			var stub = sandbox.stub(shared.ServerUtil, "listen");
-			var callback = function(){};
-			
+			var callback = function() {};
 			// when
 			sms.receive(callback);
-			
+
 			// then
 			assert(stub.calledOnce);
 			assert(stub.calledWith("http://localhost:80/sms"));
-		});
+		})
 		
 		it("should listen on parameter URL", function() {
 			// given
 			var stub = sandbox.stub(shared.ServerUtil, "listen");
-			var callback = function(){};
-			
+			var callback = function() {};
 			// when
 			sms.receive(callback, {
-				url: "http://localhost:8181/listen"
+				url : "http://localhost:8181/listen"
 			});
-			
+
 			// then
 			assert(stub.calledOnce);
 			assert(stub.calledWith("http://localhost:8181/listen"));
-		});	
+		})
+		
+		it("should call callback", function(done) {
+			// given
+			var api_response = {
+				"to" : "to",
+				"body" : "body",
+				"access_token" : "access_token",
+				"transaction_id" : "transaction_id",
+				"token_secret" : "token_secret"
+			};
+			var stub = sandbox.stub(shared.ServerUtil, "listen", function(url, callback) {
+				callback(api_response);
+			});
+			var callback = function(data) {
+				assert.deepEqual(data, api_response);
+				done();
+			};
+			
+			// when
+			sms.receive(callback);
+		})
+		
+		it("should log if response not valid", function() {
+			// given
+			var spy = sandbox.spy(shared.Logger, "warn");
+			var stub = sandbox.stub(shared.ServerUtil, "listen", function(url, callback) {
+				callback("this is not JSON");
+			});
+			var callback_spy = sinon.spy();
+			
+			// when
+			sms.receive(callback_spy);
+			
+			// then
+			assert(spy.calledOnce);
+			assert.equal(callback_spy.callCount, 0);
+		})
+		
+		it("should copy OAuthv2 access token", function(done) {
+			// given
+			shared.oAuthv2 = true;
+			var api_response = {
+				"oauthv2_access_token" : "oauth2_token",
+			};
+			var stub = sandbox.stub(shared.ServerUtil, "listen", function(url, callback) {
+				callback(api_response);
+			});
+			var callback = function(data) {
+				assert.equal("oauth2_token", data.access_token);
+				done();
+			};
+			
+			// when
+			sms.receive(callback);
+		})
 	});
 })
